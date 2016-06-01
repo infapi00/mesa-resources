7e50efb15b29c46661bf708e087ced12a2c336c5
Alejandro Piñeiro <apinheiro@igalia.com>
2016-06-01 11:06:37 +0200

    install-basic-dependencies: add more dependencies

--- install-basic-dependencies.sh
+++ install-basic-dependencies.sh
@@ -1,5 +1,7 @@
-apt-get install libtool libpthread-stubs0-dev libpciaccess-dev \
+apt-get install git autoconf autopoint pkg-config yelp-tools gettext \
+     libtool libpthread-stubs0-dev libpciaccess-dev \
      libxau-dev xutils-dev x11proto-xext-dev x11proto-input-dev \
      bison flex python-mako x11proto-dri2-dev libxext-dev \
      libxdamage-dev libudev-dev libexpat1-dev libxrandr-dev libxmu-dev \
-     zlib1g-dev libpng12-dev libgcrypt11-dev libexpat1-dev
+     zlib1g-dev libpng-dev libgcrypt11-dev libexpat1-dev \
+     libwayland-dev g++ cmake
