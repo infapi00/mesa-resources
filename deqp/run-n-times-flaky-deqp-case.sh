901674f27019b5d19beb89b6811b448a95b71200
Alejandro Piñeiro <apinheiro@igalia.com>
2016-09-02 13:37:45 +0200

    deqp: Add run-n-times-flaky-deqp-case.sh script

---
+++ deqp/run-n-times-flaky-deqp-case.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]
+then
+    printf "Error - wrong number of parameters"
+    printf "Syntax : $0 number command"
+    exit 1
+fi
+n=$1
+shift
+PASS=0
+FAIL=0
+for (( i=1; i <= "$n"; i++ ));
+do
+    if [ $($@ | grep "Passed:        1/1 (100.00%)" | wc -l) -eq 1 ]
+    then
+        ((PASS=PASS+1))
+        mv "TestResults.qpa" "TestResults_PASS"$PASS".qpa"
+    else
+        ((FAIL=FAIL+1))
+        mv "TestResults.qpa" "TestResults_FAIL"$FAIL".qpa"
+    fi
+done
+
+printf "Runs: %i, Pass: %i, Fail: %i\n" $n $PASS $FAIL
+if [ $((n-PASS)) -ne $FAIL ]
+then
+    #This can happens because we are only checking for PASS, but there
+    #are other status
+    printf "Warning: pass+fail is different that the total number of runs\n"
+fi
+
