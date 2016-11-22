3a0be77a9cfef49bce8b61e19dd98920cf8d6d89
Alejandro Piñeiro <apinheiro@igalia.com>
2016-11-22 09:07:53 -0200

    install-basic-dependencies: add a fedora one, rename previous for ubuntu

--- install-basic-dependencies-ubuntu.sh
+++ install-basic-dependencies-ubuntu.sh
@@ -4,4 +4,4 @@ apt-get install git autoconf autopoint pkg-config yelp-tools gettext \
      bison flex python-mako x11proto-dri2-dev libxext-dev \
      libxdamage-dev libudev-dev libexpat1-dev libxrandr-dev libxmu-dev \
      zlib1g-dev libpng-dev libgcrypt11-dev libexpat1-dev \
-     libwayland-dev g++ cmake
+     libwayland-dev g++ cmake python-git clang-format-3.9
