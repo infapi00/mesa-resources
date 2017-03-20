8a421d259c92f8b941197dccf9a00fc3718d31cc
Andres Gomez <agomez@igalia.com>
2017-03-20 14:40:11 +0200

    testing: added small enhancements and documentation

--- testing/full-piglit-run.sh
+++ testing/full-piglit-run.sh
@@ -1,24 +1,91 @@
 #!/bin/bash
+#
+# This script uses piglit to run a whole test suite (VK-GL-CTS, dEQP,
+# piglit) against the currently installed GL driver.
+#
+# Follow the steps below to use:
+#
+# 1. If needed tweak the DISPLAY env variable.
+# 2. Set the paths to the piglit, vk-gl-cts and deqp builds.
+# 3. Choose the prefixes to use for each test suite.
+# 4. Set the paths to existing reference results for each test suite.
+# 5. Choose which test suites to run.
+# 6. Whether to create or not a summary against the reference run.
+#
+# Run:
+#
+# $ ./full-piglit-run.sh
+
+
+# SETTINGS
+# ========
 
 export DISPLAY=:0.0
 
-TIMESTAMP=`date +%Y%m%d%H%M%S`
+# Paths ...
+# ---------
+
 DEV_PATH="/home/guest/agomez/jhbuild"
 PIGLIT="${DEV_PATH}/piglit.git"
-PIGLIT_RESULTS="${DEV_PATH}/piglit-results"
+PIGLIT_REPORTS="${DEV_PATH}/piglit-results"
 VK_GL_CTS_BUILD="${DEV_PATH}/vk-gl-cts.git/build"
 DEQP_BUILD="${DEV_PATH}/android-deqp/external/deqp/"
+
+# Prefixes:
+# ---------
+
+VK_GL_CTS_PREFIX="VK-GL-CTS"
+DEQP_GLES2_PREFIX="DEQP2"
+DEQP_GLES3_PREFIX="DEQP3"
+DEQP_GLES31_PREFIX="DEQP31"
+PIGLIT_PREFIX="all"
+
+# Reference run results:
+# ----------------------
+
+VK_GL_CTS_REFERENCE="${PIGLIT_REPORTS}/reference/${VK_GL_CTS_PREFIX}-20161118173616"
+DEQP_GLES2_REFERENCE="${PIGLIT_REPORTS}/reference/${DEQP_GLES2_PREFIX}-20160927194922"
+DEQP_GLES3_REFERENCE="${PIGLIT_REPORTS}/reference/${DEQP_GLES3_PREFIX}-20160927233138"
+DEQP_GLES31_REFERENCE="${PIGLIT_REPORTS}/reference/${DEQP_GLES31_PREFIX}-20160928061503"
+PIGLIT_REFERENCE="${PIGLIT_REPORTS}/reference/${PIGLIT_PREFIX}-20160928132005"
+
+# What tests to run?
+# ------------------
+
 RUN_VK_GL_CTS=true
 RUN_DEQP_GLES2=false
 RUN_DEQP_GLES3=false
 RUN_DEQP_GLES31=false
 RUN_PIGLIT=false
-VK_GL_CTS_REFERENCE=reference/VK-GL-GL45-CTS-20161118173616
-DEQP_GLES2_REFERENCE=reference/DEQP2-20160927194922
-DEQP_GLES3_REFERENCE=reference/DEQP3-20160927233138
-DEQP_GLES31_REFERENCE=reference/DEQP31-20160928061503
-PIGLIT_REFERENCE=reference/all-20160928132005
+
+# Create a report against the reference result?
+# ---------------------------------------------
+
 CREATE_PIGLIT_REPORT=false
+
+
+# RUNNING
+# =======
+
+TIMESTAMP=`date +%Y%m%d%H%M%S`
+
+VK_GL_CTS_NAME="${VK_GL_CTS_PREFIX}-${TIMESTAMP}"
+DEQP_GLES2_NAME="${DEQP_GLES2_PREFIX}-${TIMESTAMP}"
+DEQP_GLES3_NAME="${DEQP_GLES3_PREFIX}-${TIMESTAMP}"
+DEQP_GLES31_NAME="${DEQP_GLES31_PREFIX}-${TIMESTAMP}"
+PIGLIT_NAME="${PIGLIT_PREFIX}-${TIMESTAMP}"
+
+VK_GL_CTS_RESULTS="${PIGLIT_REPORTS}/results/${VK_GL_CTS_NAME}"
+DEQP_GLES2_RESULTS="${PIGLIT_REPORTS}/results/${DEQP_GLES2_NAME}"
+DEQP_GLES3_RESULTS="${PIGLIT_REPORTS}/results/${DEQP_GLES3_NAME}"
+DEQP_GLES31_RESULTS="${PIGLIT_REPORTS}/results/${DEQP_GLES31_NAME}"
+PIGLIT_RESULTS="${PIGLIT_REPORTS}/results/${PIGLIT_NAME}"
+
+VK_GL_CTS_SUMMARY="${PIGLIT_REPORTS}/summary/${VK_GL_CTS_NAME}"
+DEQP_GLES2_SUMMARY="${PIGLIT_REPORTS}/summary/${DEQP_GLES2_NAME}"
+DEQP_GLES3_SUMMARY="${PIGLIT_REPORTS}/summary/${DEQP_GLES3_NAME}"
+DEQP_GLES31_SUMMARY="${PIGLIT_REPORTS}/summary/${DEQP_GLES31_NAME}"
+PIGLIT_SUMMARY="${PIGLIT_REPORTS}/summary/${PIGLIT_NAME}"
 
 ( ! ${RUN_VK_GL_CTS} \
     || ( echo PIGLIT_CTS_GL_BIN="${VK_GL_CTS_BUILD}"/external/openglcts/modules/glcts \
@@ -26,63 +93,65 @@ CREATE_PIGLIT_REPORT=false
               MESA_GLES_VERSION_OVERRIDE=3.2 \
               MESA_GL_VERSION_OVERRIDE=4.5 \
               MESA_GLSL_VERSION_OVERRIDE=450 \
-             "${PIGLIT}"/piglit run cts_gl -t GL45 -n VK-GL-GL45-CTS-BDW-GT2-"${TIMESTAMP}" "${PIGLIT_RESULTS}"/results/VK-GL-GL45-CTS-BDW-GT2-"${TIMESTAMP}" \
+             "${PIGLIT}"/piglit run cts_gl -t GL45 -n "${VK_GL_CTS_NAME}" "${VK_GL_CTS_RESULTS}" \
          && PIGLIT_CTS_GL_BIN="${VK_GL_CTS_BUILD}"/external/openglcts/modules/glcts \
             PIGLIT_CTS_GL_EXTRA_ARGS="--deqp-case=GL45*" \
             MESA_GLES_VERSION_OVERRIDE=3.2 \
             MESA_GL_VERSION_OVERRIDE=4.5 \
             MESA_GLSL_VERSION_OVERRIDE=450 \
-            "${PIGLIT}"/piglit run cts_gl -t GL45 -n VK-GL-GL45-CTS-BDW-GT2-"${TIMESTAMP}" "${PIGLIT_RESULTS}"/results/VK-GL-GL45-CTS-BDW-GT2-"${TIMESTAMP}" \
+            "${PIGLIT}"/piglit run cts_gl -t GL45 -n "${VK_GL_CTS_NAME}" "${VK_GL_CTS_RESULTS}" \
          && unset PIGLIT_CTS_GL_BIN \
          && unset PIGLIT_CTS_GL_EXTRA_ARGS \
          && unset MESA_GLSL_VERSION_OVERRIDE \
          && unset MESA_GLSL_VERSION_OVERRIDE \
          && ( ! ${CREATE_PIGLIT_REPORT} \
-                || "${PIGLIT}"/piglit summary html --overwrite "${PIGLIT_RESULTS}"/summary/VK-GL-GL45-CTS-BDW-GT2 "${PIGLIT_RESULTS}"/"${VK_GL_CTS_REFERENCE}" "${PIGLIT_RESULTS}"/results/VK-GL-GL45-CTS-BDW-GT2-"${TIMESTAMP}" ) ) ) \
+                || "${PIGLIT}"/piglit summary html --overwrite "${PIGLIT_SUMMARY}" "${VK_GL_CTS_REFERENCE}" "${PIGLIT_REPORTS}" ) ) ) \
   && ( ! ${RUN_DEQP_GLES2} \
     || ( echo PIGLIT_DEQP_GLES2_BIN="${DEQP_BUILD}"/modules/gles2/deqp-gles2 \
               PIGLIT_DEQP_GLES2_EXTRA_ARGS="--deqp-visibility hidden" \
-              "${PIGLIT}"/piglit run deqp_gles2 -t dEQP-GLES2 "${PIGLIT_RESULTS}"/results/DEQP2-"${TIMESTAMP}" \
+              "${PIGLIT}"/piglit run deqp_gles2 -t dEQP-GLES2 -n "${DEQP_GLES2_NAME}" "${DEQP_GLES2_RESULTS}" \
          && PIGLIT_DEQP_GLES2_BIN="${DEQP_BUILD}"/modules/gles2/deqp-gles2 \
             PIGLIT_DEQP_GLES2_EXTRA_ARGS="--deqp-visibility hidden" \
-            "${PIGLIT}"/piglit run deqp_gles2 -t dEQP-GLES2 "${PIGLIT_RESULTS}"/results/DEQP2-"${TIMESTAMP}" \
+            "${PIGLIT}"/piglit run deqp_gles2 -t dEQP-GLES2 -n "${DEQP_GLES2_NAME}" "${DEQP_GLES2_RESULTS}" \
          && unset PIGLIT_DEQP_GLES2_BIN \
          && unset PIGLIT_DEQP_GLES2_EXTRA_ARGS \
          && ( ! ${CREATE_PIGLIT_REPORT} \
-                || "${PIGLIT}"/piglit summary html --overwrite "${PIGLIT_RESULTS}"/summary/DEQP2 "${PIGLIT_RESULTS}"/"${DEQP2_REFERENCE}" "${PIGLIT_RESULTS}"/results/DEQP2-"${TIMESTAMP}" ) ) ) \
+                || "${PIGLIT}"/piglit summary html --overwrite "${DEQP_GLES2_SUMMARY}" "${DEQP_GLES2_REFERENCE}" "${DEQP_GLES2_RESULTS}" ) ) ) \
   && ( ! ${RUN_DEQP_GLES3} \
     || ( echo PIGLIT_DEQP_GLES3_EXE="${DEQP_BUILD}"/modules/gles3/deqp-gles3 \
               PIGLIT_DEQP_GLES3_EXTRA_ARGS="--deqp-visibility hidden" \
-              "${PIGLIT}"/piglit run deqp_gles3 -t dEQP-GLES3 "${PIGLIT_RESULTS}"/results/DEQP3-"${TIMESTAMP}" \
+              "${PIGLIT}"/piglit run deqp_gles3 -t dEQP-GLES3 -n "${DEQP_GLES3_NAME}" "${DEQP_GLES3_RESULTS}" \
          && PIGLIT_DEQP_GLES3_EXE="${DEQP_BUILD}"/modules/gles3/deqp-gles3 \
             PIGLIT_DEQP_GLES3_EXTRA_ARGS="--deqp-visibility hidden" \
-            "${PIGLIT}"/piglit run deqp_gles3 -t dEQP-GLES3 "${PIGLIT_RESULTS}"/results/DEQP3-"${TIMESTAMP}" \
+            "${PIGLIT}"/piglit run deqp_gles3 -t dEQP-GLES3 -n "${DEQP_GLES3_NAME}" "${DEQP_GLES3_RESULTS}" \
          && unset PIGLIT_DEQP_GLES3_EXE \
          && unset PIGLIT_DEQP_GLES3_EXTRA_ARGS \
          && ( ! ${CREATE_PIGLIT_REPORT} \
-                || "${PIGLIT}"/piglit summary html --overwrite "${PIGLIT_RESULTS}"/summary/DEQP3 "${PIGLIT_RESULTS}"/"${DEQP3_REFERENCE}" "${PIGLIT_RESULTS}"/results/DEQP3-"${TIMESTAMP}" ) ) ) \
+                || "${PIGLIT}"/piglit summary html --overwrite "${DEQP_GLES3_SUMMARY}" "${DEQP_GLES3_REFERENCE}" "${DEQP_GLES3_RESULTS}" ) ) ) \
   && ( ! ${RUN_DEQP_GLES31} \
     || ( echo MESA_GLES_VERSION_OVERRIDE=3.1 \
               PIGLIT_DEQP_GLES31_BIN="${DEQP_BUILD}"/modules/gles31/deqp-gles31 \
               PIGLIT_DEQP_GLES31_EXTRA_ARGS="--deqp-visibility hidden" \
-              "${PIGLIT}"/piglit run deqp_gles31 -t dEQP-GLES31 "${PIGLIT_RESULTS}"/results/DEQP31-"${TIMESTAMP}" \
+              "${PIGLIT}"/piglit run deqp_gles31 -t dEQP-GLES31 -n "${DEQP_GLES31_NAME}" "${DEQP_GLES31_RESULTS}" \
          && MESA_GLES_VERSION_OVERRIDE=3.1 \
             PIGLIT_DEQP_GLES31_BIN="${DEQP_BUILD}"/modules/gles31/deqp-gles31 \
             PIGLIT_DEQP_GLES31_EXTRA_ARGS="--deqp-visibility hidden" \
-            "${PIGLIT}"/piglit run deqp_gles31 -t dEQP-GLES31 "${PIGLIT_RESULTS}"/results/DEQP31-"${TIMESTAMP}" \
+            "${PIGLIT}"/piglit run deqp_gles31 -t dEQP-GLES31 -n "${DEQP_GLES31_NAME}" "${DEQP_GLES31_RESULTS}" \
          && unset PIGLIT_DEQP_GLES31_BIN \
          && unset PIGLIT_DEQP_GLES31_EXTRA_ARGS \
          && unset MESA_GLES_VERSION_OVERRIDE \
          && ( ! ${CREATE_PIGLIT_REPORT} \
-                || "${PIGLIT}"/piglit summary html --overwrite "${PIGLIT_RESULTS}"/summary/DEQP31 "${PIGLIT_RESULTS}"/"${DEQP31_REFERENCE}" "${PIGLIT_RESULTS}"/results/DEQP31-"${TIMESTAMP}" ) ) ) \
+                || "${PIGLIT}"/piglit summary html --overwrite "${DEQP_GLES31_SUMMARY}" "${DEQP_GLES31_REFERENCE}" "${DEQP_GLES31_RESULTS}" ) ) ) \
   && ( ! ${RUN_PIGLIT} \
-    || "${PIGLIT}"/piglit run all -x texcombine -x texCombine -n all-"${TIMESTAMP}" ../piglit-results/results/all-"${TIMESTAMP}" \
+    || "${PIGLIT}"/piglit run all -x texcombine -x texCombine -n all-"${PIGLIT_NAME}" "${PIGLIT_SUMMAR}Y" \
        && ( ! ${CREATE_PIGLIT_REPORT} \
-              || "${PIGLIT}"/piglit summary html --overwrite "${PIGLIT_RESULTS}"/summary/all "${PIGLIT_RESULTS}"/"${PIGLIT_REFERENCE}" "${PIGLIT_RESULTS}"/results/all-"${TIMESTAMP}" ) )
+              || "${PIGLIT}"/piglit summary html --overwrite "${PIGLIT_SUMMARY}" "${PIGLIT_REFERENCE}" "${PIGLIT_RESULTS}" ) )
 
 exit $?
 
 
 
 # Reminder compilation line for VK-GL-CTS
+# ---------------------------------------
+
 # cmake .. -DCMAKE_C_FLAGS="-Werror -Wno-error=unused-command-line-argument -m64" -DCMAKE_CXX_FLAGS="-Werror -Wno-error=unused-command-line-argument -m64" -DCMAKE_C_COMPILER=clang-3.9 -DCMAKE_CXX_COMPILER=clang++-3.9 -DDEQP_TARGET=x11_egl -DGLCTS_GTF_TARGET=gles32 -DCMAKE_LIBRARY_PATH=/home/agomez/devel/graphics/install/lib/ -DCMAKE_INCLUDE_PATH=/home/agomez/devel/graphics/install/include/
--- testing/run-bisect.sh
+++ testing/run-bisect.sh
@@ -1,30 +1,89 @@
 #!/bin/bash
+#
+# This script runs a specific tests from a test suite against mesa, in
+# which an ongoing bisect is happening.
+#
+# Follow the steps below to use:
+#
+# 1. If needed tweak the DISPLAY env variable.
+# 2. Set the paths to the mesa, piglit and vk-gl-cts builds.
+# 3. Choose whether you want to force the rebuild of the test suite.
+#
+# Run a bisect. This is a simple example of usage:
+#
+# <path_to_mesa>$ git bisect start
+# <path_to_mesa>$ git bisect bad    # Current version is bad
+# <path_to_mesa>$ git bisect good <known_good_revision>
+# <path_to_mesa>$ git bisect run <path_to>/run-bisect.sh GL44-CTS.shading_language_420pack.initializer_list_negative
+#
+# It will stop either when:
+#    1. A compilation in mesa fails (error code 128).
+#    2. An installation in mesa fails (error code 129).
+#       * Optionally, the compilation of the CTS tests fail with a
+#         specific mesa version (error code 130).
+#    3. It has found the last good version of mesa for the specific
+#       CTS test.
+#
+# In the 2 first steps it will need interactive action from the user.
+# Usually just:
+#
+# <path_to_mesa>$ git bisect skip
+#
+# Normally, it only will stop when 3. is reached.
+
+
+# SETTINGS
+# ========
 
 export DISPLAY=:0.0
 
-CTSTEST=$1
+# Paths ...
+# ---------
 
-TIMESTAMP=`date +%Y%m%d%H%M%S`
 DEV_PATH="/home/guest/agomez/jhbuild"
 MESA="${DEV_PATH}/mesa.git"
 PIGLIT="${DEV_PATH}/piglit.git"
 VK_GL_CTS_BUILD="${DEV_PATH}/vk-gl-cts.git/build"
 
+# Rebuild the test suite?
+# -----------------------
+
+FORCE_REBUILD=false
+
+
+# RUNNING
+# =======
+
+usage()
+{
+    echo -e "\e[31mUSAGE:"
+    echo -e "\e[31m$0 <test>"
+}
+
+if ! [ $1 ]
+then
+    usage
+    exit 131
+fi
+
+TIMESTAMP=`date +%Y%m%d%H%M%S`
+CTSTEST=$1
 RESULT=0
 until false; do
   cd "${MESA}"
-  # Rebuilding shouldn't be necessary
-#  make -j12 || (RESULT=128 && break)
-#  make install || (RESULT=129 && break)
+  make -j12 || (RESULT=128 && break)
+  make install || (RESULT=129 && break)
   cd -
   cd "${VK_GL_CTS_BUILD}"
-  # Rebuilding shouldn't be necessary
-#  cmake --build . || (RESULT=130 && break)
+  if [ $FORCE_REBUILD ]; then
+      # Rebuilding shouldn't be necessary but we can force it ...
+      cmake --build . || (RESULT=130 && break)
+  fi
   cd -
-  echo 'MESA_GLES_VERSION_OVERRIDE=3.2 MESA_GL_VERSION_OVERRIDE="4.5" MESA_GLSL_VERSION_OVERRIDE="450" PIGLIT_SOURCE_DIR='"${PIGLIT}"' PIGLIT_PLATFORM="mixed_glx_egl" '"${VK_GL_CTS_BUILD}"'/cts/glcts --deqp-case='"${CTSTEST}"
+  echo 'MESA_GLES_VERSION_OVERRIDE=3.2 MESA_GL_VERSION_OVERRIDE="4.5" MESA_GLSL_VERSION_OVERRIDE="450" '"${VK_GL_CTS_BUILD}"'/cts/glcts --deqp-case='"${CTSTEST}"
   # Duplicate stdout
   exec 5>&1
-  CTS_OUTPUT=$(MESA_GLES_VERSION_OVERRIDE=3.2 MESA_GL_VERSION_OVERRIDE="4.5" MESA_GLSL_VERSION_OVERRIDE="450" PIGLIT_SOURCE_DIR="${PIGLIT}" PIGLIT_PLATFORM="mixed_glx_egl" "${VK_GL_CTS_BUILD}"/external/openglcts/modules/glcts --deqp-case="${CTSTEST}" | tee >(cat - >&5))
+  CTS_OUTPUT=$(MESA_GLES_VERSION_OVERRIDE=3.2 MESA_GL_VERSION_OVERRIDE="4.5" MESA_GLSL_VERSION_OVERRIDE="450" "${VK_GL_CTS_BUILD}"/external/openglcts/modules/glcts --deqp-case="${CTSTEST}" | tee >(cat - >&5))
   echo "${CTS_OUTPUT}" | grep "Passed" | grep "100.00%"
   RESULT=$?
   break
