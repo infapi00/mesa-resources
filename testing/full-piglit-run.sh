787cb8562c7b2f84d5d86c96bd55fbba613324b2
Andres Gomez <agomez@igalia.com>
2017-04-26 23:16:38 +0300

    testing: piglit run -l quiet is not really quiet so redirecting output
    Actually -l quiet is the default mode, which is not quiet at all.
    
    Using output redirection to silence piglit.

--- testing/full-piglit-run.sh
+++ testing/full-piglit-run.sh
@@ -164,7 +164,9 @@ if ${FPR_RUN_PIGLIT}; then
 fi
 
 if ${FPR_VERBOSE}; then
-    PIGLIT_RUN_QUIET="-l quiet"
+    FPR_OUTPUT=1
+else
+    FPR_OUTPUT=/dev/null
 fi
 
 TIMESTAMP=`date +%Y%m%d%H%M%S`
@@ -198,13 +200,12 @@ DEQP_GLES31_REFERENCE="${FPR_PIGLIT_REPORTS_PATH}/reference/${FPR_DEQP_GLES31_PR
 PIGLIT_REFERENCE="${FPR_PIGLIT_REPORTS_PATH}/reference/${FPR_PIGLIT_PREFIX}-${GL_DRIVER}${FPR_PIGLIT_REFERENCE_SUFFIX:+-}${FPR_PIGLIT_REFERENCE_SUFFIX}"
 
 ( ! ${FPR_RUN_VK_CTS} \
-    || ( ( ! ${FPR_VERBOSE} \
-	    || echo PIGLIT_DEQP_VK_BIN="${FPR_VK_GL_CTS_BUILD_PATH}"/external/vulkancts/modules/vulkan/deqp-vk  \
-		    PIGLIT_DEQP_VK_EXTRA_ARGS="--deqp-log-images=disable --deqp-log-shader-sources=disable" \
-		    "${FPR_PIGLIT_PATH}"/piglit run ${PIGLIT_RUN_QUIET} deqp_vk -n "${VK_CTS_NAME}" "${VK_CTS_RESULTS}" ) \
+    || ( echo PIGLIT_DEQP_VK_BIN="${FPR_VK_GL_CTS_BUILD_PATH}"/external/vulkancts/modules/vulkan/deqp-vk  \
+	      PIGLIT_DEQP_VK_EXTRA_ARGS="--deqp-log-images=disable --deqp-log-shader-sources=disable" \
+	      "${FPR_PIGLIT_PATH}"/piglit run deqp_vk -n "${VK_CTS_NAME}" "${VK_CTS_RESULTS}" >&"${FPR_OUTPUT}" 2>&1 \
          && PIGLIT_DEQP_VK_BIN="${FPR_VK_GL_CTS_BUILD_PATH}"/external/vulkancts/modules/vulkan/deqp-vk \
             PIGLIT_DEQP_VK_EXTRA_ARGS="--deqp-log-images=disable --deqp-log-shader-sources=disable" \
-            "${FPR_PIGLIT_PATH}"/piglit run ${PIGLIT_RUN_QUIET} deqp_vk -n "${VK_CTS_NAME}" "${VK_CTS_RESULTS}" \
+            "${FPR_PIGLIT_PATH}"/piglit run deqp_vk -n "${VK_CTS_NAME}" "${VK_CTS_RESULTS}" >&"${FPR_OUTPUT}" 2>&1 \
          && unset PIGLIT_DEQP_VK_BIN \
          && unset PIGLIT_DEQP_VK_EXTRA_ARGS \
          && ( ! ${FPR_CREATE_PIGLIT_REPORT} \
@@ -215,19 +216,18 @@ PIGLIT_REFERENCE="${FPR_PIGLIT_REPORTS_PATH}/reference/${FPR_PIGLIT_PREFIX}-${GL
 					&& echo "${SUMMARY}" \
 					&& "${FPR_PIGLIT_PATH}"/piglit summary html -o -e pass "${PIGLIT_SUMMARY}" "${VK_CTS_REFERENCE}" "${FPR_PIGLIT_REPORTS_PATH}" > /dev/null 2>&1 ) ) ) ) ) \
   && ( ! ${FPR_RUN_GL_CTS} \
-    || ( ( ! ${FPR_VERBOSE} \
-	    || echo PIGLIT_CTS_GL_BIN="${FPR_VK_GL_CTS_BUILD_PATH}"/external/openglcts/modules/glcts \
-		    PIGLIT_CTS_GL_EXTRA_ARGS="--deqp-case=GL45*" \
-		    MESA_GLES_VERSION_OVERRIDE=3.2 \
-		    MESA_GL_VERSION_OVERRIDE=4.5 \
-		    MESA_GLSL_VERSION_OVERRIDE=450 \
-		    "${FPR_PIGLIT_PATH}"/piglit run ${PIGLIT_RUN_QUIET} cts_gl -t GL45 -n "${GL_CTS_NAME}" "${GL_CTS_RESULTS}" ) \
+    || ( echo PIGLIT_CTS_GL_BIN="${FPR_VK_GL_CTS_BUILD_PATH}"/external/openglcts/modules/glcts \
+	      PIGLIT_CTS_GL_EXTRA_ARGS="--deqp-case=GL45*" \
+	      MESA_GLES_VERSION_OVERRIDE=3.2 \
+	      MESA_GL_VERSION_OVERRIDE=4.5 \
+	      MESA_GLSL_VERSION_OVERRIDE=450 \
+	      "${FPR_PIGLIT_PATH}"/piglit run cts_gl -t GL45 -n "${GL_CTS_NAME}" "${GL_CTS_RESULTS}" >&"${FPR_OUTPUT}" 2>&1 \
          && PIGLIT_CTS_GL_BIN="${FPR_VK_GL_CTS_BUILD_PATH}"/external/openglcts/modules/glcts \
             PIGLIT_CTS_GL_EXTRA_ARGS="--deqp-case=GL45*" \
             MESA_GLES_VERSION_OVERRIDE=3.2 \
             MESA_GL_VERSION_OVERRIDE=4.5 \
             MESA_GLSL_VERSION_OVERRIDE=450 \
-            "${FPR_PIGLIT_PATH}"/piglit run ${PIGLIT_RUN_QUIET} cts_gl -t GL45 -n "${GL_CTS_NAME}" "${GL_CTS_RESULTS}" \
+            "${FPR_PIGLIT_PATH}"/piglit run cts_gl -t GL45 -n "${GL_CTS_NAME}" "${GL_CTS_RESULTS}" >&"${FPR_OUTPUT}" 2>&1 \
          && unset PIGLIT_CTS_GL_BIN \
          && unset PIGLIT_CTS_GL_EXTRA_ARGS \
          && unset MESA_GLES_VERSION_OVERRIDE \
@@ -241,15 +241,14 @@ PIGLIT_REFERENCE="${FPR_PIGLIT_REPORTS_PATH}/reference/${FPR_PIGLIT_PREFIX}-${GL
 					&& echo "${SUMMARY}" \
 					&& "${FPR_PIGLIT_PATH}"/piglit summary html -o -e pass "${PIGLIT_SUMMARY}" "${GL_CTS_REFERENCE}" "${FPR_PIGLIT_REPORTS_PATH}" > /dev/null 2>&1 ) ) ) ) ) \
   && ( ! ${FPR_RUN_DEQP_GLES2} \
-    || ( ( ! ${FPR_VERBOSE} \
-	    || echo PIGLIT_DEQP_GLES2_BIN="${FPR_DEQP_BUILD_PATH}"/modules/gles2/deqp-gles2 \
-		    PIGLIT_DEQP_GLES2_EXTRA_ARGS="--deqp-visibility hidden" \
-		    MESA_GLES_VERSION_OVERRIDE=2.0 \
-		    "${FPR_PIGLIT_PATH}"/piglit run ${PIGLIT_RUN_QUIET} deqp_gles2 -t dEQP-GLES2 -n "${DEQP_GLES2_NAME}" "${DEQP_GLES2_RESULTS}" ) \
+    || ( echo PIGLIT_DEQP_GLES2_BIN="${FPR_DEQP_BUILD_PATH}"/modules/gles2/deqp-gles2 \
+	      PIGLIT_DEQP_GLES2_EXTRA_ARGS="--deqp-visibility hidden" \
+	      MESA_GLES_VERSION_OVERRIDE=2.0 \
+	      "${FPR_PIGLIT_PATH}"/piglit run deqp_gles2 -t dEQP-GLES2 -n "${DEQP_GLES2_NAME}" "${DEQP_GLES2_RESULTS}" >&"${FPR_OUTPUT}" 2>&1 \
          && PIGLIT_DEQP_GLES2_BIN="${FPR_DEQP_BUILD_PATH}"/modules/gles2/deqp-gles2 \
             PIGLIT_DEQP_GLES2_EXTRA_ARGS="--deqp-visibility hidden" \
             MESA_GLES_VERSION_OVERRIDE=2.0 \
-            "${FPR_PIGLIT_PATH}"/piglit run ${PIGLIT_RUN_QUIET} deqp_gles2 -t dEQP-GLES2 -n "${DEQP_GLES2_NAME}" "${DEQP_GLES2_RESULTS}" \
+            "${FPR_PIGLIT_PATH}"/piglit run deqp_gles2 -t dEQP-GLES2 -n "${DEQP_GLES2_NAME}" "${DEQP_GLES2_RESULTS}" >&"${FPR_OUTPUT}" 2>&1 \
          && unset PIGLIT_DEQP_GLES2_BIN \
          && unset PIGLIT_DEQP_GLES2_EXTRA_ARGS \
          && unset MESA_GLES_VERSION_OVERRIDE \
@@ -261,15 +260,14 @@ PIGLIT_REFERENCE="${FPR_PIGLIT_REPORTS_PATH}/reference/${FPR_PIGLIT_PREFIX}-${GL
 					&& echo "${SUMMARY}" \
 					&& "${FPR_PIGLIT_PATH}"/piglit summary html -o -e pass "${DEQP_GLES2_SUMMARY}" "${DEQP_GLES2_REFERENCE}" "${DEQP_GLES2_RESULTS}" > /dev/null 2>&1 ) ) ) ) ) \
   && ( ! ${FPR_RUN_DEQP_GLES3} \
-    || ( ( ! ${FPR_VERBOSE} \
-	    || echo PIGLIT_DEQP_GLES3_EXE="${FPR_DEQP_BUILD_PATH}"/modules/gles3/deqp-gles3 \
-		    PIGLIT_DEQP_GLES3_EXTRA_ARGS="--deqp-visibility hidden" \
-		    MESA_GLES_VERSION_OVERRIDE=3.0 \
-		    "${FPR_PIGLIT_PATH}"/piglit run ${PIGLIT_RUN_QUIET} deqp_gles3 -t dEQP-GLES3 -n "${DEQP_GLES3_NAME}" "${DEQP_GLES3_RESULTS}" ) \
+    || ( echo PIGLIT_DEQP_GLES3_EXE="${FPR_DEQP_BUILD_PATH}"/modules/gles3/deqp-gles3 \
+	      PIGLIT_DEQP_GLES3_EXTRA_ARGS="--deqp-visibility hidden" \
+	      MESA_GLES_VERSION_OVERRIDE=3.0 \
+	      "${FPR_PIGLIT_PATH}"/piglit run deqp_gles3 -t dEQP-GLES3 -n "${DEQP_GLES3_NAME}" "${DEQP_GLES3_RESULTS}" >&"${FPR_OUTPUT}" 2>&1 \
          && PIGLIT_DEQP_GLES3_EXE="${FPR_DEQP_BUILD_PATH}"/modules/gles3/deqp-gles3 \
             PIGLIT_DEQP_GLES3_EXTRA_ARGS="--deqp-visibility hidden" \
             MESA_GLES_VERSION_OVERRIDE=3.0 \
-            "${FPR_PIGLIT_PATH}"/piglit run ${PIGLIT_RUN_QUIET} deqp_gles3 -t dEQP-GLES3 -n "${DEQP_GLES3_NAME}" "${DEQP_GLES3_RESULTS}" \
+            "${FPR_PIGLIT_PATH}"/piglit run deqp_gles3 -t dEQP-GLES3 -n "${DEQP_GLES3_NAME}" "${DEQP_GLES3_RESULTS}" >&"${FPR_OUTPUT}" 2>&1 \
          && unset PIGLIT_DEQP_GLES3_EXE \
          && unset PIGLIT_DEQP_GLES3_EXTRA_ARGS \
          && unset MESA_GLES_VERSION_OVERRIDE \
@@ -281,15 +279,14 @@ PIGLIT_REFERENCE="${FPR_PIGLIT_REPORTS_PATH}/reference/${FPR_PIGLIT_PREFIX}-${GL
 					&& echo "${SUMMARY}" \
 					&& "${FPR_PIGLIT_PATH}"/piglit summary html -o -e pass "${DEQP_GLES3_SUMMARY}" "${DEQP_GLES3_REFERENCE}" "${DEQP_GLES3_RESULTS}" > /dev/null 2>&1 ) ) ) ) ) \
   && ( ! ${FPR_RUN_DEQP_GLES31} \
-    || ( ( ! ${FPR_VERBOSE} \
-	    || echo PIGLIT_DEQP_GLES31_BIN="${FPR_DEQP_BUILD_PATH}"/modules/gles31/deqp-gles31 \
-		    PIGLIT_DEQP_GLES31_EXTRA_ARGS="--deqp-visibility hidden" \
-		    MESA_GLES_VERSION_OVERRIDE=3.1 \
-		    "${FPR_PIGLIT_PATH}"/piglit run ${PIGLIT_RUN_QUIET} deqp_gles31 -t dEQP-GLES31 -n "${DEQP_GLES31_NAME}" "${DEQP_GLES31_RESULTS}" ) \
+    || ( echo PIGLIT_DEQP_GLES31_BIN="${FPR_DEQP_BUILD_PATH}"/modules/gles31/deqp-gles31 \
+	      PIGLIT_DEQP_GLES31_EXTRA_ARGS="--deqp-visibility hidden" \
+	      MESA_GLES_VERSION_OVERRIDE=3.1 \
+	      "${FPR_PIGLIT_PATH}"/piglit run deqp_gles31 -t dEQP-GLES31 -n "${DEQP_GLES31_NAME}" "${DEQP_GLES31_RESULTS}" ) >&"${FPR_OUTPUT}" 2>&1 \
          && PIGLIT_DEQP_GLES31_BIN="${FPR_DEQP_BUILD_PATH}"/modules/gles31/deqp-gles31 \
             PIGLIT_DEQP_GLES31_EXTRA_ARGS="--deqp-visibility hidden" \
             MESA_GLES_VERSION_OVERRIDE=3.1 \
-	    "${FPR_PIGLIT_PATH}"/piglit run ${PIGLIT_RUN_QUIET} deqp_gles31 -t dEQP-GLES31 -n "${DEQP_GLES31_NAME}" "${DEQP_GLES31_RESULTS}" \
+	    "${FPR_PIGLIT_PATH}"/piglit run deqp_gles31 -t dEQP-GLES31 -n "${DEQP_GLES31_NAME}" "${DEQP_GLES31_RESULTS}" >&"${FPR_OUTPUT}" 2>&1 \
          && unset PIGLIT_DEQP_GLES31_BIN \
          && unset PIGLIT_DEQP_GLES31_EXTRA_ARGS \
          && unset MESA_GLES_VERSION_OVERRIDE \
@@ -301,16 +298,15 @@ PIGLIT_REFERENCE="${FPR_PIGLIT_REPORTS_PATH}/reference/${FPR_PIGLIT_PREFIX}-${GL
 					&& echo "${SUMMARY}" \
 					&& "${FPR_PIGLIT_PATH}"/piglit summary html -o -e pass "${DEQP_GLES31_SUMMARY}" "${DEQP_GLES31_REFERENCE}" "${DEQP_GLES31_RESULTS}" > /dev/null 2>&1 ) ) ) ) ) \
   && ( ! ${FPR_RUN_PIGLIT} \
-    || ( ( ! ${FPR_VERBOSE} \
-	    || echo "${FPR_PIGLIT_PATH}"/piglit run ${PIGLIT_RUN_QUIET} all -x texcombine -x texCombine -n "${PIGLIT_NAME}" "${PIGLIT_RESULTS}" ) \
-	       && "${FPR_PIGLIT_PATH}"/piglit run ${PIGLIT_RUN_QUIET} all -x texcombine -x texCombine -n "${PIGLIT_NAME}" "${PIGLIT_RESULTS}" \
-	       && ( ! ${FPR_CREATE_PIGLIT_REPORT} \
-			  || ( ( SUMMARY=$("${FPR_PIGLIT_PATH}"/piglit summary console -d "${PIGLIT_REFERENCE}" "${PIGLIT_RESULTS}") \
-				       && read -ra RESULTS <<< $(echo "${SUMMARY}" | grep ^regressions) \
-				       && test "x${RESULTS[2]}" = "x0" ) \
-				   || ( echo "${PIGLIT_NAME}" \
-					      && echo "${SUMMARY}" \
-					      && "${FPR_PIGLIT_PATH}"/piglit summary html -e pass -o "${PIGLIT_SUMMARY}" "${PIGLIT_REFERENCE}" "${PIGLIT_RESULTS}" > /dev/null 2>&1 ) ) ) ) )
+    || ( echo "${FPR_PIGLIT_PATH}"/piglit run all -x texcombine -x texCombine -n "${PIGLIT_NAME}" "${PIGLIT_RESULTS}" >&"${FPR_OUTPUT}" 2>&1 \
+	     && "${FPR_PIGLIT_PATH}"/piglit run all -x texcombine -x texCombine -n "${PIGLIT_NAME}" "${PIGLIT_RESULTS}" >&"${FPR_OUTPUT}" 2>&1 \
+	     && ( ! ${FPR_CREATE_PIGLIT_REPORT} \
+			|| ( ( SUMMARY=$("${FPR_PIGLIT_PATH}"/piglit summary console -d "${PIGLIT_REFERENCE}" "${PIGLIT_RESULTS}") \
+				     && read -ra RESULTS <<< $(echo "${SUMMARY}" | grep ^regressions) \
+				     && test "x${RESULTS[2]}" = "x0" ) \
+				 || ( echo "${PIGLIT_NAME}" \
+					    && echo "${SUMMARY}" \
+					    && "${FPR_PIGLIT_PATH}"/piglit summary html -e pass -o "${PIGLIT_SUMMARY}" "${PIGLIT_REFERENCE}" "${PIGLIT_RESULTS}" > /dev/null 2>&1 ) ) ) ) )
 
 exit $?
 
