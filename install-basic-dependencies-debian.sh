d72178b5f01b6e4e11c5f3ad45e183fb82d96866
Alejandro Piñeiro <apinheiro@igalia.com>
2019-06-03 12:34:47 +0200

    install-basic-dependencies: simplify debian script
    The purpose of those scripts is install the most basic libraries
    needed to start to develop with mesa. So mostly mesa dependencies and
    little more. So removing stuff like mesa/vim that should be more
    developer taste.

--- install-basic-dependencies-debian.sh
+++ install-basic-dependencies-debian.sh
@@ -1,15 +1,13 @@
 
 apt-get install libxcb-randr0-dev libxrandr-dev libxcb-xinerama0-dev \
         libxinerama-dev libxcursor-dev libxcb-cursor-dev libxkbcommon-dev \
-        python-numpy python3-numpy bash-completion python-optcomplete libpng-dev \
-        libpng-tools autoconf autotools-dev pkg-config pkgconf \
-        cmake libtool libtool-bin gettext gettext-base autopoint \
+        python-numpy python3-numpy libpng-dev libpng-tools autoconf autotools-dev \
+        pkg-config pkgconf cmake libtool libtool-bin gettext gettext-base autopoint \
         yelp-tools xutils-dev xutils-dev libpthread-stubs0-dev libpciaccess-dev \
-        valgrind libffi-dev expat libexpat1-dev libxml2-dev x11proto-xext-dev \
+        valgrind libffi-dev expat libexpat1-dev x11proto-xext-dev \
         xmlto libxcb1-dev libxcb-*dev bison flex python-mako libssl-dev \
         libgnutls28-dev x11proto-dri2-dev x11proto-dri3-dev libx11-dev \
         libxcb-glx0-dev libx11-xcb-dev libxext-dev libxdamage-dev libxfixes-dev \
-        llvm-dev libva-dev glew-utils libglew-dev x11proto-randr-dev emacs-nox \
-        emacs x11proto-present-dev libclc-dev libelf-dev libclang-dev sudo \
-        intel-microcode firmware-linux git build-essential firmware-iwlwifi \
-        python-gtk2-dev vim apitrace mesa-utils libvulkan-dev
+        llvm-dev libva-dev x11proto-randr-dev  \
+        x11proto-present-dev libclc-dev libelf-dev libclang-dev sudo \
+        git build-essential mesa-utils libvulkan-dev
