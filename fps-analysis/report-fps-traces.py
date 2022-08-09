59ca9a75dabae1bd6ad88284cf7ec6710cbb25fc
Alejandro Piñeiro <apinheiro@igalia.com>
2022-08-09 13:06:53 +0200

    fps-analysis: add run-all-traces.py and report-fps-traces.py scripts
    Utility scripts heavily based on the shader-db run/report scripts:
    
       * run-all-traces: runs gfxrecon and apitrace traces from a
         directory, and write the output to a results directory.
    
         The main use case it to gather fps changes after a change on
         mesa.
    
       * report-fps-traces: used to compare and write a summary report of
         two different FPS outcomes of run-all-traces.py script.

---
+++ fps-analysis/report-fps-traces.py
@@ -0,0 +1,258 @@
+#!/usr/bin/env python3
+
+import re
+import argparse
+import math
+import statistics
+import csv
+from scipy import stats
+import numpy as np
+import pathlib
+import sys
+
+def gather_statistics(changes, before, after, m):
+    stats = (0.0, 0, 0.0, 0, 0, 0.0, 0.0, 0.0, 0.0, 0.0)
+
+    if changes:
+        absolute = [abs(before[p][m] - after[p][m]) for p in changes]
+        relative = [0 if before[p][m] == 0 else abs(before[p][m] - after[p][m]) / before[p][m] for p in changes]
+
+        stats = (statistics.mean(absolute),
+                 statistics.median(absolute),
+                 min(absolute),
+                 max(absolute),
+                 statistics.mean(relative),
+                 statistics.median(relative),
+                 min(relative),
+                 max(relative))
+
+    return stats
+
+
+def format_percent(frac):
+    """Converts a factional value (typically 0.0 to 1.0) to a string as a percentage"""
+    if abs(frac) > 0.0 and abs(frac) < 0.0001:
+        return "<.01%"
+    else:
+        return "{:.2f}%".format(frac * 100)
+
+
+def get_delta(b, a):
+    if b != 0 and a != 0:
+        frac = float(a) / float(b) - 1.0
+        return ' ({})'.format(format_percent(frac))
+    else:
+        return ''
+
+
+def format_num(n):
+    assert n >= 0
+    if n - math.floor(n) < 0.01:
+        return str(math.floor(n))
+    else:
+        return "{:.2f}".format(n)
+
+
+def change(b, a):
+    return format_num(b) + " -> " + format_num(a) + get_delta(b, a)
+
+
+def get_result_string(p, b, a):
+    p = p + ": "
+    while len(p) < 50:
+        p = p + ' '
+    return p + change(b, a)
+
+
+def get_results(filename):
+    file = open(filename, "r")
+    lines = file.read().split('\n')
+
+    results = {}
+
+    # Each line has the format "trace_name,fps", and can be more that one trace_name entry
+    with open(filename) as file_obj:
+        reader_obj = csv.reader(file_obj)
+        for row in reader_obj:
+            if row[0] in results:
+                result_list = results[row[0]]
+            else:
+                result_list = []
+
+            result_list.append(float(row[1]))
+            results[row[0]] = result_list
+
+    return results
+
+
+def process_results(raw, args):
+    results = {}
+
+    for key in raw:
+        fps_list = raw[key]
+        fps_accum = 0
+        fps_max = 0
+        fps_min = sys.float_info.max
+
+        for fps in raw[key]:
+            fps_accum += fps
+            fps_max = max(fps, fps_max)
+            fps_min = min(fps, fps_min)
+
+        result_group = {}
+        result_group['fps_min'] = fps_min
+        result_group['fps_max'] = fps_max
+        result_group['fps_avg'] = fps_accum / len(raw[key])
+        results[key] = result_group
+
+    return results
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("before", help="The output of the original code")
+    parser.add_argument("after", help="The output of the new code")
+    parser.add_argument("--summary-only", "-s", action="store_true", default=False,
+                        help="Do not show the trace helped / hurt data")
+    parser.add_argument("--skip-gfxrecon", action="store_true", help="If we should skip gfxreconstruct traces")
+    parser.add_argument("--skip-apitrace", action="store_true", help="If we should skip apitrace traces")
+
+    args = parser.parse_args()
+
+    measurements = ["fps_min", "fps_max", "fps_avg"]
+
+    before_raw = get_results(args.before)
+    before = process_results(before_raw, args)
+    after_raw = get_results(args.after)
+    after = process_results(after_raw, args)
+
+    total_before = {}
+    total_after = {}
+    affected_before = {}
+    affected_after = {}
+    helped_statistics = {}
+    hurt_statistics = {}
+    num_hurt = {}
+    num_helped = {}
+
+    #FIXME: not measuring confidence intervals. It is not clear if
+    #that is representative here, and in case of being, we need to
+    #tweak the values for fps
+
+    # Filling up helper/hurt
+    for m in measurements:
+        helped = []
+        hurt = []
+
+        total_before[m] = 0
+        total_after[m] = 0
+        affected_before[m] = 0
+        affected_after[m] = 0
+
+        for p in before:
+            file_extension = pathlib.Path(p).suffix
+            if (file_extension == '.gfxr' and args.skip_gfxrecon):
+                continue
+
+            if (file_extension == '.trace' and args.skip_apitrace):
+                continue
+
+            before_count = before[p][m]
+
+            if after.get(p) is None:
+                continue
+
+            after_count = after[p][m]
+
+            total_before[m] += before_count
+            total_after[m] += after_count
+
+            if before_count != after_count:
+                affected_before[m] += before_count
+                affected_after[m] += after_count
+
+                # Measuring only FPS, higher is always better
+                if (after_count > before_count):
+                    helped.append(p)
+                else:
+                    hurt.append(p)
+
+        if not args.summary_only:
+            helped.sort(
+                key=lambda k: after[k][m] if before[k][m] == 0 else float(before[k][m] - after[k][m]) / before[k][m])
+            for p in helped:
+                namestr = p
+                print(m + " helped:   " +
+                      get_result_string(namestr, before[p][m], after[p][m]))
+            if helped:
+                print("")
+
+            hurt.sort(
+                key=lambda k: after[k][m] if before[k][m] == 0 else float(after[k][m] - before[k][m]) / before[k][m])
+            for p in hurt:
+                namestr = p
+                print(m + " HURT:   " +
+                      get_result_string(namestr, before[p][m], after[p][m]))
+            if hurt:
+                print("")
+
+
+        helped_statistics[m] = gather_statistics(helped, before, after, m)
+        hurt_statistics[m] = gather_statistics(hurt, before, after, m)
+
+        num_helped[m] = len(helped)
+        num_hurt[m] = len(hurt)
+
+    lost = []
+    gained = []
+
+    for p in before:
+        if after.get(p) is None:
+            lost.append(p)
+
+    for p in after:
+        if before.get(p) is None:
+            gained.append(p)
+
+    if not args.summary_only:
+        lost.sort()
+        for p in lost:
+            print("LOST:   " + p)
+        if lost:
+            print("")
+
+        gained.sort()
+        for p in gained:
+            print("GAINED: " + p)
+        if gained:
+            print("")
+
+    any_helped_or_hurt = False
+    for m in measurements:
+        if num_helped[m] > 0 or num_hurt[m] > 0:
+            any_helped_or_hurt = True
+
+        if num_helped[m] > 0 or num_hurt[m] > 0:
+            print("total {0} in shared programs: {1}\n"
+                  "{0} in affected programs: {2}\n"
+                  "helped: {3}\n"
+                  "HURT: {4}".format(
+                      m,
+                      change(total_before[m], total_after[m]),
+                      change(affected_before[m], affected_after[m]),
+                      num_helped[m],
+                      num_hurt[m]))
+
+            # FIXME: not printing the abs/rel statistics
+            print()
+
+
+    if lost or gained:
+        print("LOST:   " + str(len(lost)))
+        print("GAINED: " + str(len(gained)))
+    else:
+        if not any_helped_or_hurt:
+            print("No changes.")
+
+if __name__ == "__main__":
+    main()
---
+++ fps-analysis/run-all-traces.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python3
+#
+# Runs all the gfxrecon captures to check for fps measures
+#
+# As usually this is done after a mesa rebuild, we do this twice, to
+# check the fps measures with a hot shader disk cache.
+#
+import argparse
+import os
+import pathlib
+import re
+import shutil
+import subprocess
+import sys
+
+# Sometimes we get a xcb failures on the gfxrecon replay, that we are
+# not sure how to fix or when we would be able to work on it. For now
+# we just retry a maximum amount of attempts.
+#
+# FIXME: hardcoded, perhaps a new command line parameter?
+
+MAX_ATTEMPTS = 3
+
+def run_trace(args, filename, fps_file, num_samples):
+    #FIXME: add a verbose mode?
+    print("Current trace: " + filename)
+
+    file_extension = pathlib.Path(filename).suffix
+    if file_extension == '.gfxr':
+        if args.skip_gfxrecon:
+            print("\tskipped")
+            return
+
+        command = ['gfxrecon-replay']
+        command += ['--log-level', 'fatal']
+        if args.rebind:
+            command += ['-m', 'rebind']
+    elif file_extension == '.trace':
+        if args.skip_apitrace:
+            print("\tskipped")
+            return
+        command = ['apitrace']
+        command += ['replay']
+        command += ['-b']
+    else:
+        print("File extension " + file_extension + " not recognized")
+        return
+
+    command += [filename]
+    remaining_attempts = MAX_ATTEMPTS
+
+    for sample in range(num_samples):
+        if remaining_attempts <= 0:
+            continue
+        for attempt in range(remaining_attempts):
+            try:
+                output = subprocess.run(command,
+                                        capture_output=True,
+                                        check=True)
+
+                # Parse FPS value. Note that the stdout of the
+                # gfxrecon-replay is not always the same, as for some
+                # samples there are no load time stats, so we use regex to
+                # find the string with "Replay FPS" header.
+
+                split = output.stdout.splitlines()
+                fps = 0.0
+
+                # FIXME: this seems somewhat convoluted. I bet that
+                # there is way to get the FPS from the string with
+                # just one regex, without an index, and without
+                # tweaking based on gfxr vs apitrace. This works for
+                # now.
+                for line in split:
+                    if file_extension == '.gfxr':
+                        search = re.compile('Replay FPS')
+                        fps_index = 0
+                    else:
+                        search = re.compile('Rendered')
+                        fps_index = 2
+                    line_str = line.decode('utf-8')
+                    match = search.search(line_str)
+                    if match is not None:
+                        float_search = re.findall(r"[-+]?\d*\.\d+|\d+", line_str)
+                        if float_search is not None:
+                            fps = float(float_search[fps_index])
+
+                if fps_file is not None:
+                    new_line = os.path.basename(filename)
+                    new_line += ',' + str(fps)
+                    new_line += '\n'
+                    fps_file.write(new_line)
+
+            except Exception as err:
+                print("ERROR executing trace " + filename + " : " + f"{type(err).__name__} was raised: {err}")
+                if attempt < MAX_ATTEMPTS:
+                    print("\tRetrying to execute trace " + filename + " Remaining attempts " + str(remaining_attempts))
+                remaining_attempts = remaining_attempts - 1
+            else:
+                break
+        else:
+            print("Consumed " + str(MAX_ATTEMPTS) + " attempts for trace " + filename + ". Discarding trace")
+
+# Note that although we provide a number of samples on the command
+# line arguments, we still need to pass it as a parameter, in order to
+# configure the initial cache warmup with a value of 1.
+def run_traces(args, traces_directory, fps_file, num_samples):
+    for filename in os.listdir(traces_directory):
+        f = os.path.join(traces_directory, filename)
+        if os.path.isfile(f):
+            run_trace(args, f, fps_file, num_samples)
+
+def run_helper(args, traces_directory, results_directory):
+    if os.path.exists(results_directory):
+        shutil.rmtree(results_directory)
+    os.mkdir(results_directory)
+
+    if args.disable_cache_run is False:
+        #FIXME: add a verbose mode?
+        print("Warming up shader cache")
+        run_traces(args, traces_directory, None, 1)
+
+    fps_file = open(results_directory + '/fps-stats.txt', 'w')
+
+    #FIXME: add a verbose mode?
+    print("Starting FPS run")
+    run_traces(args, traces_directory, fps_file, args.num_samples)
+    fps_file.close()
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--rebind", nargs='?', default=False, type=bool, help="If we call gfxrecon-replay with -m rebind")
+    parser.add_argument("--disable-cache-run", nargs='?', default=False, type=bool, help="By default we do a first run to ensure a hot shader cache, not included for the fps stats")
+    parser.add_argument("--traces-directory", nargs='?', default="traces", type=str, help="Directory with the traces")
+    parser.add_argument("--num-samples", nargs='?', default=1, type=int, help="Number of times each trace is executed to get the (averaged) fps value. Not include the shaderdb run")
+    parser.add_argument("--skip-gfxrecon", action="store_true", help="If we should skip running the gfxreconstruct traces")
+    parser.add_argument("--skip-apitrace", action="store_true", help="If we should skip running the apitrace traces")
+    parser.add_argument("--mesa-directory", nargs='?', type=str, help="Mesa directory. Useful if we pass a list of mesa commits")
+    parser.add_argument("--mesa-commit-list", nargs='+', action="extend", type=str, help="List of mesa commits to execute the script against")
+
+    args = parser.parse_args()
+
+    if args.mesa_commit_list is not None and len(args.mesa_commit_list) > 10:
+        print("List of mesa commits too big (maximum value is 10)")
+        return
+
+    if args.skip_gfxrecon and args.skip_apitrace:
+        print("Both --skip-gfxrecon and --skip-apitrace options used. Nothing to do")
+        return
+
+    traces_directory = os.path.expanduser(args.traces_directory)
+    print(traces_directory)
+
+    # We ensure that the on-disk-cache is enabled, as we want hot-cache fps numbers
+    os.unsetenv("MESA_GLSL_CACHE_DISABLE")
+    os.unsetenv("MESA_SHADER_CACHE_DISABLE")
+
+    # FIXME: perhaps further configuration on the results folder
+    # through a command line option?
+    base_results_directory = "results"
+
+    if (args.mesa_commit_list is None):
+        run_helper(args, traces_directory, base_results_directory)
+    else:
+        index = 0
+        for commit in args.mesa_commit_list:
+            results_directory = base_results_directory + "-" + str(index) + "-" + commit
+            mesa_directory = os.path.expanduser(args.mesa_directory)
+            command =  ['git', 'checkout', commit]
+            print(command)
+
+            try:
+                output = subprocess.run(command,
+                                        cwd=mesa_directory)
+            except Exception as err:
+                print("ERROR moving to commit  " + commit + " : " + f"{type(err).__name__} was raised: {err}")
+                return
+
+            #FIXME: this only works if you are using jhbuild to build mesa
+            try:
+                output = subprocess.run(['jhbuild', 'buildone', 'mesa', '-fn'])
+            except Exception as err:
+                print("ERROR building mesa  " + commit + " : " + f"{type(err).__name__} was raised: {err}")
+                return
+
+            run_helper(args, traces_directory, results_directory)
+
+            try:
+                output = subprocess.run(['git', 'switch', '-'],
+                                        cwd=mesa_directory)
+            except Exception as err:
+                print("ERROR moving back to current mesa branch : " + f"{type(err).__name__} was raised: {err}")
+                return
+
+            index = index + 1
+
+
+if __name__ == "__main__":
+    main()
+
