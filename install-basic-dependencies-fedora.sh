5ab10c59592a9e8a910ad10ebc8291e649b88c32
Alejandro Piñeiro <apinheiro@igalia.com>
2023-02-23 13:26:58 +0100

    install-basic-dependencies: remove all those scripts
    Packages names changes through debian/ubuntu/fedora releases. Also the
    dependencies required by mesa (and the mesa dependencies included on
    the moduleset) also changed.
    
    Right now those scripts are outdated. All attempts to use them
    recently failed. No plans to keep updating them, so let's remove them.

--- install-basic-dependencies-fedora.sh
+++
@@ -1,4 +0,0 @@
-dnf install automake autoconf yelp-tools libtool xorg-x11-util-macros libxcb-devel \
-    libpciaccess-devel bison flex python-mako libXext-devel libXdamage-devel \
-    expat-devel libgcrypt-devel gcc-c++ cmake python3-numpy mesa-libGLU-devel \
-    zlib-devel libpng-devel GitPython python-requests clang vulkan-headers
--- install-basic-dependencies-ubuntu.sh
+++
@@ -1,8 +0,0 @@
-apt-get install git autoconf autopoint pkg-config yelp-tools gettext \
-     libtool libpthread-stubs0-dev libpciaccess-dev \
-     libxau-dev xutils-dev x11proto-xext-dev x11proto-input-dev \
-     bison flex python-mako x11proto-dri2-dev libxext-dev \
-     libxdamage-dev libudev-dev libexpat1-dev libxrandr-dev libxmu-dev \
-     zlib1g-dev libpng-dev libgcrypt11-dev libexpat1-dev \
-     libwayland-dev g++ cmake python-git clang-format-3.9 \
-     python-numpy python3-numpy libvulkan-dev ninja-build
