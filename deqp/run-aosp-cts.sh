54ba5eeeccc89949b5423d27fb474c57b13dbd2f
Alejandro Piñeiro <apinheiro@igalia.com>
2026-04-08 10:40:44 +0200

    deqp: use rgba8888d24s8ms0 for aosp runs
    As that script is intended to use one supported configuration, let's
    explicitly set one used on conformance run. This is needed to avoid
    several failures when using the Wayland target, as in that case right
    now a R10G10B10A2 framebuffer format is used, and right now several
    tests fails on that situation.

--- deqp/run-aosp-cts.sh
+++ deqp/run-aosp-cts.sh
@@ -2,10 +2,10 @@
 #
 # This script is a wrapper over deqp-runner to run all the aosp tests
 # for gles. We don't include gl, as for aosp, it goes up to 4.5. Note
-# that this would run only with the default configuration
+# that this only run the rgba8888d24s8ms0  configuration
 
-deqp-runner run --deqp ./glcts --output gles2-aosp.log  --caselist ~/mesa/source/vk-gl-cts/external/openglcts/data/gl_cts/data/mustpass/gles/aosp_mustpass/main/gles2-main.txt
-deqp-runner run --deqp ./glcts --output gles3-aosp.log  --caselist ~/mesa/source/vk-gl-cts/external/openglcts/data/gl_cts/data/mustpass/gles/aosp_mustpass/main/gles3-main.txt
-deqp-runner run --deqp ./glcts --output gles31-aosp.log  --caselist ~/mesa/source/vk-gl-cts/external/openglcts/data/gl_cts/data/mustpass/gles/aosp_mustpass/main/gles31-main.txt
+deqp-runner run --deqp ./glcts --output gles2-aosp.log --caselist ~/mesa/source/vk-gl-cts/external/openglcts/data/gl_cts/data/mustpass/gles/aosp_mustpass/main/gles2-main.txt -- --deqp-gl-config-name=rgba8888d24s8ms0
+deqp-runner run --deqp ./glcts --output gles3-aosp.log --caselist ~/mesa/source/vk-gl-cts/external/openglcts/data/gl_cts/data/mustpass/gles/aosp_mustpass/main/gles3-main.txt -- --deqp-gl-config-name=rgba8888d24s8ms0
+deqp-runner run --deqp ./glcts --output gles31-aosp.log --caselist ~/mesa/source/vk-gl-cts/external/openglcts/data/gl_cts/data/mustpass/gles/aosp_mustpass/main/gles31-main.txt -- --deqp-gl-config-name=rgba8888d24s8ms0
 
 cat gles2-aosp.log/failures.csv gles3-aosp.log/failures.csv gles31-aosp.log/failures.csv > all-aosp-cts-failures.csv
