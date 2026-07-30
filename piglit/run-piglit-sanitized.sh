5451138bb4295f75a966f03b999934c16be7d80b
Alejandro Piñeiro <apinheiro@igalia.com>
2026-07-30 17:42:26 +0200

    piglit/sanitized: add two more tests to the OOM skip list
    Lately we got issues when running the sanitized script concurrently
    (with the -c option), on devices with ~8GB (rpi5, rock5b). Although it
    doesn't happen always, as it depends on which tests are running at
    that time, we found that adding them makes the run finishing
    consistenly without raising a system OOM (destkop restart
    included). The tests added are:
    
      * spec/arb_texture_multisample/large-float-texture-array
      * spec/arb_texture_multisample/large-float-texture-array-fp16
    
    Note that spec/arb_texture_multisample/large-float-texture-fp16 was
    already skipped, so adding those two shouldn't be a surprise. Also far
    better in practice that setting up the numbers of jobs.
    
    As we are here we move
    spec/arb_texture_multisample/large-float-texture to the OOM skip list,
    and improve slightly the description of the test.

--- piglit/run-piglit-sanitized.sh
+++ piglit/run-piglit-sanitized.sh
@@ -5,12 +5,18 @@
 #
 # This is tailored for embedded drivers, like v3d and panfrost, so it
 # also do a skip of extensions that usually are not supported on those
-# drivers. We noted that helped on the time required for the full run.
+# drivers, as pre-filtering is far quicker that skipping test-by-test
+# when running them.
+#
+# All that makes this script useful for smoke-testing on a day-by-day
+# basis with a really good range of features and tests on a reasonable
+# time.
+#
 
 PIGLITSKIPPEDTESTS="-x arb_gpu_shader_fp64 -x arb_gpu_shader_int64 -x arb_gpu_shader5 -x arb_vertex_attrib_64bit -x arb_shader_image_load_store -x arb_shader_group_vote -x arb_shader_image_size -x  arb_shader_precision -x arb_shader_stencil_export -x arb_shader_storage_buffer_object -x arb_shader_subroutine -x arb_shading_language_packing -x arb_sparse_buffer -x arb_tessellation_shader -x arb_texture_barrier -x arb_texture_cube_map_array -x arb_texture_gather -x arb_texture_query_lod -x ext_shader_framebuffer_fetch -x ext_shader_framebuffer_fetch_non_coherent -x amd_conservative_depth -x amd_depth_clamp_separate -x amd_vertex_shader_layer -x amd_vertex_shader_viewport_index -x arb_bindless_texture -x arb_blend_func_extended -x arb_shader_ballot -x arb_shader_clock -x arb_shader_draw_parameters -x arb_timer_query -x arb_transform_feedback_overflow_query -x arb_viewport_array -x egl_android_native_fence_sync -x tcs- -x tessellation -x \\\.tese -x \\\.tesc  -x \\\@tes -x \\\@tcs"
-PIGLITUNSTABLETESTS="-x max-texture-size -x glx-multi-context-single-window -x arb_texture_multisample@large-float-texture-fp16 -x longprim"
+PIGLITUNSTABLETESTS="-x max-texture-size -x glx-multi-context-single-window -x longprim"
 PIGLITSLOWTESTS="-x tex3d-maxsize -x texture-env-combine -x glx-visuals-depth -x glx-visuals-stencil"
-PIGLITOOMTESTS="-x streaming-texture-leak"
+PIGLITOOMTESTS="-x streaming-texture-leak -x arb_texture_multisample@large-float-texture-fp16 -x arb_texture_multisample@large-float-texture-array -x arb_texture_multisample@large-float-texture-array-fp16"
 
 ./piglit run -c -l verbose --timeout 90 --overwrite $PIGLITOOMTESTS $PIGLITUNSTABLETESTS $PIGLITSLOWTESTS $PIGLITSKIPPEDTESTS all ../piglit_results
 
